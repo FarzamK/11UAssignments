0114ddee1ee64870ec90632853f62f2c2a0c42ef
kathf6052 <kathf6052@HRH-IC0027338.wrdsb.ca>
2017-10-10 13:18:44 -0400

    A4q4

--- src/Assignment4/A4Q4.java
+++ src/Assignment4/A4Q4.java
@@ -18,7 +18,35 @@ public class A4Q4 {
     public static void main(String[] args) {
         
         Scanner promCost = new Scanner(System.in);
-
+        Scanner djCost = new Scanner(System.in);
+        Scanner rentCost = new Scanner(System.in);
+        Scanner decCost = new Scanner(System.in);
+        Scanner staffCost = new Scanner(System.in);
+        Scanner miscCost = new Scanner(System.in);
         
+        System.out.println("How much does the food for prom cost?");
+        int prom = promCost.nextInt();
+        
+        System.out.println("How much does the DJ cost?");
+        int dj = djCost.nextInt();
+        
+        System.out.println("How much does it cost to rent the hall");
+        int rent = rentCost.nextInt();
+        
+        System.out.println("How much does decoration cost?");
+        int dec = decCost.nextInt();
+        
+        System.out.println("How much does it cost for staff?");
+        int staff = staffCost.nextInt();
+        
+        System.out.println("How much for miscellaneous costs?");
+        int misc = miscCost.nextInt();
+        
+        
+        int totalCost = prom + dj + rent + dec + staff + misc;
+        
+        int totalTickets = (int)Math.ceil(totalCost/35.0);
+        
+        System.out.println("The total cost is $" + totalCost + ". You will need to sell " + totalTickets + " tickets to break even.");
     }
 }
