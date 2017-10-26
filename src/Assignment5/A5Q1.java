0a6a20bbb0cda9e84d75b27f3152f3a91834afce
kathf6052 <kathf6052@HRH-IC0027338.wrdsb.ca>
2017-10-26 13:10:42 -0400

    almost done a5

---
+++ src/Assignment5/A5Q1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kathf6052
+ */
+public class A5Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a word ");
+        String word = input.nextLine();
+
+
+        for (int i = 0; i < word.length(); i++) {
+
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'o') {
+                String front = word.substring(0, i);
+                String ending = word.substring(i);
+
+                word = front + "ub" + ending;
+
+
+                i = i + 3;
+
+
+
+            }
+
+
+
+        }
+        System.out.println(word);
+    }
+}
