0cefd5704efb63563fb4f55b87efb6ca1321752b
kathf6052 <kathf6052@HRH-IC0027338.wrdsb.ca>
2017-10-31 14:22:30 -0400

    Done a5. Just need to comment

--- src/Assignment5/A5Q1.java
+++ src/Assignment5/A5Q1.java
@@ -16,32 +16,44 @@ public class A5Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        while (true) {
+            // create a new scanner
             Scanner input = new Scanner(System.in);
-
+            // create a boolean and make it equal false 
+            boolean vowel = false;
             System.out.println("Please enter a word ");
+            // create a sting that the user will input
             String word = input.nextLine();
-
-
+            // create a blank string that will be edited by the string builder
+            String translated = "";
+            // create a string builder
+            StringBuilder sb = new StringBuilder(translated);
+            // for loop to add to the 
             for (int i = 0; i < word.length(); i++) {
 
-                if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'o') {
-                    String front = word.substring(0, i);
-                    String ending = word.substring(i);
-
-                    word = front + "ub" + ending;
-
-
-                    i = i + 3;
+                if ((word.charAt(i) == 'a' || word.charAt(i) == 'e'|| word.charAt(i) == 'u' || word.charAt(i) == 'i'|| word.charAt(i) == 'o') && vowel == false) {
+                    sb.append("ub");
+                    sb.append(word.charAt(i));
+                    vowel = true;
+                    
+                    
 
 
 
+
+
+                }else if((word.charAt(i) == 'a' || word.charAt(i) == 'e'|| word.charAt(i) == 'u' || word.charAt(i) == 'i'|| word.charAt(i) == 'o') && vowel == true){
+                   sb.append(word.charAt(i));
+                   
+                }else{
+                    sb.append(word.charAt(i));
+                    vowel = false;
+                    
+                   
+                    
                 }
-
-
-
+                
             }
-            System.out.println(word);
+            System.out.println(sb);
         }
     }
-}
+
