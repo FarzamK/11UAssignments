184d1ff02319a0063e44c3ed0b5a027a659d8ffc
kathf6052 <kathf6052@HRH-IC0027338.wrdsb.ca>
2017-12-01 14:25:04 -0500

    a7q2

--- src/Assignment7/A7Q3.java
+++ src/Assignment7/A7Q3.java
@@ -12,6 +12,17 @@ import java.util.Scanner;
  */
 public class A7Q3 {
     public void factor(int number){
+        // for loop
+        for (int i = 1; i < number; i++){
+            
+            // if statement to do the math
+            if(number % i == 0){
+                System.out.println(i);
+            }
+            
+        }
+        // display out the number
+        System.out.println(number);
         
         
     }
@@ -19,7 +30,21 @@ public class A7Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // create object to run methods
+        A7Q3 run = new A7Q3();
         // create a scanner
         Scanner in = new Scanner(System.in);
+        // ask the user for the number
+        System.out.println("Please enter an integer that you would like the factors of");
+        // create a variable and assign what the user entered
+        int integer = in.nextInt();
+        // display 
+        System.out.println("The factors of " + integer + " are: ");
+        // run the method 
+        run.factor(integer);
+        
+
+        
+        
     }
 }
